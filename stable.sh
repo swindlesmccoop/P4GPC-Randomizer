6f09e6a2f898eef8951f81b3889cd12b42bd7c30
Swindles McCoop <65102316+swindlesmccoop@users.noreply.github.com>
2021-05-10 12:05:47 -0400

    Update stable.sh

--- stable.sh
+++ stable.sh
@@ -3,3 +3,11 @@
 #PLEASE READ THE README FIRST!!!!!!!!!!!!!
 
 #Run this script at your own risk. I am not responsible for an unplayable experience.
+
+function randomize {
+paste <(printf "%s\n" *) <(printf "%s\n" * | shuf) |
+  while IFS=$'\t' read -r from to; do mv -- "$from" "$to.new"; done
+for f in *.new; do mv -- "$f" "${f%.new}"; done 
+}
+
+#Start randomization
