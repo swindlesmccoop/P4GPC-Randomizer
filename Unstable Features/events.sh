8f66ccc9b12f583f4176008c5bbbc3fdea6a79a0
Swindles McCoop <65102316+swindlesmccoop@users.noreply.github.com>
2021-05-19 11:11:16 -0400

    Update events.sh

--- Unstable Features/events.sh
+++ Unstable Features/events.sh
@@ -2,6 +2,7 @@
 
 #this will randomize all .pm1 files. i have no idea if it will work, so it's going into unstable features for now.
 #i'm also too lazy to verify that all of the folder names here exist.
+#i've forgotten a lot about how the game plays cutscenes so maybe this will permanently break saves? i dont remember if it uses pm1 or bf.
 
 function randomize {
 paste <(printf "%s\n" *) <(printf "%s\n" * | shuf) |
