f351887b3f781f890b46ef8d3ef073c2de46e043
Swindles McCoop <65102316+swindlesmccoop@users.noreply.github.com>
2021-05-19 11:43:22 -0400

    Update script.sh

--- Unstable Features/script.sh
+++ Unstable Features/script.sh
@@ -1,1 +1,23 @@
 #!/bin/sh
+
+#randomize function
+function randomize {
+paste <(printf "%s\n" *) <(printf "%s\n" * | shuf) |
+  while IFS=$'\t' read -r from to; do mv -- "$from" "$to.new"; done
+for f in *.new; do mv -- "$f" "${f%.new}"; done 
+}
+
+#im gonna separate the files based on their names so that i can prevent crashes
+cd commu/script
+mkdir chara
+mkdir dungeon
+mkdir random
+mkdir nc
+mkdir fc
+mkdir bin
+mkdir bmd
+mkdir comu
+mkdir comumsg
+mkdir sys
+
+mv aegis.bf amada.bf aragaki.bf fuka.bf junpei.bf kanji.bf kitsune.bf koromaru.bf kuma.bf mituru.bf naoto.bf rise.bf sanada.bf tae.bf yousuke.bf yukari.bf yukiko.bf chara/
