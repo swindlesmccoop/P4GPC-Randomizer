2c6f5ea8fcb02f69383e0893142ad93673cac4f9
Swindles McCoop <65102316+swindlesmccoop@users.noreply.github.com>
2021-05-19 11:09:36 -0400

    Separate bc and em pacs
    maybe this will lead to less crashes?

--- Unstable Features/bc_pacs.sh
+++ Unstable Features/bc_pacs.sh
@@ -1,1 +1,35 @@
 #!/bin/sh
+
+#randomize function
+function randomize {
+paste <(printf "%s\n" *) <(printf "%s\n" * | shuf) |
+  while IFS=$'\t' read -r from to; do mv -- "$from" "$to.new"; done
+for f in *.new; do mv -- "$f" "${f%.new}"; done 
+}
+
+#we're not gonna touch the bin or se file, not even sure what they do but don't want to randomize stuf when i have no idea the contents of the archive
+cd model/pack
+mkdir temp
+mv *.bin /temp
+mv *.se /temp
+
+#moving the em pacs into their own folder so that i can separate bc and em pacs and hopefully bring rate of crashes down
+mkdir em
+mv em*.pac /em
+
+#randomize pack and em
+randomize
+cd em
+randomize
+
+#bring files back into pack folder
+mv *.pac ..
+cd ..
+cd temp
+mv *.bin ..
+mv *.se ..
+cd ..
+
+#clean up temp folders
+rm -r /temp
+rm -r /em
