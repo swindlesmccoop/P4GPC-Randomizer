bdcbc32d75466764b3e0f0f7d013af9279adca07
Swindles McCoop <65102316+swindlesmccoop@users.noreply.github.com>
2021-05-21 11:25:04 -0400

    Update script.sh

--- Unstable Features/script.sh
+++ Unstable Features/script.sh
@@ -8,16 +8,62 @@ for f in *.new; do mv -- "$f" "${f%.new}"; done
 }
 
 #im gonna separate the files based on their names so that i can prevent crashes
-cd commu/script
-mkdir chara
-mkdir dungeon
-mkdir random
-mkdir nc
-mkdir fc
-mkdir bin
+cd field/script
+
+#make directories for randomization
 mkdir bmd
+mkdir chara
 mkdir comu
 mkdir comumsg
+mkdir dungeon
+mkdir f0
+mkdir fc
+mkdir n0
+mkdir nm
+mkdir ns
+mkdir nsbmd
+mkdir random
 mkdir sys
 
+#random
+mv costume.bf random/
+mv npccomu.bf random/
+mv village.bf random/
+mv lmap.bf random/
+
+#chara
 mv aegis.bf amada.bf aragaki.bf fuka.bf junpei.bf kanji.bf kitsune.bf koromaru.bf kuma.bf mituru.bf naoto.bf rise.bf sanada.bf tae.bf yousuke.bf yukari.bf yukiko.bf chara/
+
+#comu
+mv comu*.bf comu/
+
+#comumsg
+mv comumsg*.bin comumsg/
+
+#dungeon
+mv dungeon.bf dungeon/
+mv dungeon*.bf dungeon/
+
+#f0xx
+mv f0*.bf f0/
+
+#fc
+mv fc*.bf fc/
+
+#n0
+mv n0*_0*.bf n0/
+
+#nm
+mv nm0*_0*.bmd nm/
+
+#ns bf and bmds
+mv ns0*_*.bf ns/
+mv ns0*_*.bmd nsbmd/
+
+#new randomize function?
+for d in */; do
+    cd $d
+    randomize
+    cd ..
+done
+mv */* .
