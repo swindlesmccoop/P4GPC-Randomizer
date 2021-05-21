683258e99cb03869a7ef9dcde65ba92862293173
Swindles McCoop <65102316+swindlesmccoop@users.noreply.github.com>
2021-05-21 11:41:22 -0400

    Update table_bins.sh

--- Unstable Features/table_bins.sh
+++ Unstable Features/table_bins.sh
@@ -1,1 +1,27 @@
 #!/bin/sh
+
+#randomize function
+function randomize {
+paste <(printf "%s\n" *) <(printf "%s\n" * | shuf) |
+  while IFS=$'\t' read -r from to; do mv -- "$from" "$to.new"; done
+for f in *.new; do mv -- "$f" "${f%.new}"; done 
+}
+
+#make directories
+mkdir hbin
+mkdir nbin
+mkdir etc
+
+#move files to respective directories
+cd field/table
+mv h*.bin hbin/
+mv n*.bin nbin/
+mv *.bin etc/
+
+#randomize all folders
+for d in */; do
+    cd $d
+    randomize
+    cd ..
+done
+mv */*
