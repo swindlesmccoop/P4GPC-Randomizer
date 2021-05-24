606182325ed60fb23f2caa1b2cf49df0d13a5054
Swindles McCoop <65102316+swindlesmccoop@users.noreply.github.com>
2021-05-24 11:44:14 -0400

    Defined and used a new "poggers" function

--- Unstable Features/field.sh
+++ Unstable Features/field.sh
@@ -7,6 +7,48 @@ paste <(printf "%s\n" *) <(printf "%s\n" * | shuf) |
 for f in *.new; do mv -- "$f" "${f%.new}"; done 
 }
 
+#discrimination and randomization function
+function poggers {
+for f in `find . -type f`; do
+    mkdir -p ${f##*.}
+    mv $f ${f##*.}
+done
+for d in */; do
+    cd $d
+    randomize
+    cd ..
+done
+mv */* .
+}
+
+cd field
+poggers
+
+cd grmd
+randomize
+cd ..
+
+cd lmap_spr
+randomize
+cd ..
+
+cd map
+randomize
+cd ..
+
+cd pack
+mkdir rmd
+mkdir arc
+mv *_rmd.arc rmd/
+mv *.arc arc/
+cd rmd
+randomize
+cd ..
+cd arc
+randomize
+cd ..
+cd ..
+
 #go into the field model directory
 #not touching kasa_toumei.amd until i know what it is
 cd model/field
