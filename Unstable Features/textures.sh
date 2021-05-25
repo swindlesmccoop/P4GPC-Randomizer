46ee3d3103bd065fa5497b7736ba7a8e7b453311
Swindles McCoop <65102316+swindlesmccoop@users.noreply.github.com>
2021-05-25 12:18:03 -0400

    Update textures.sh

--- Unstable Features/textures.sh
+++ Unstable Features/textures.sh
@@ -7,5 +7,32 @@ paste <(printf "%s\n" *) <(printf "%s\n" * | shuf) |
 for f in *.new; do mv -- "$f" "${f%.new}"; done 
 }
 
+#fonts and stuff
+cd font
+mkdir jm
+mkdir spr
+mv jm_k*.spr jm/
+cd jm
+randomize
+cd ..
+mv *.spr spr/
+cd spr
+randomize
+mv *.spr ..
+cd ..
+cd jm
+mv *.spr ..
+cd ..
+rm -r jm spr
+
+#pictures, though i dont know what they are
+cd picture
+randomize
+cd ..
+cd ..
+
 cd model/npc/textures
 randomize
+cd ..
+cd ..
+cd ..
