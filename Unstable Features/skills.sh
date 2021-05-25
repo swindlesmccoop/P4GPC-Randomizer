9cf5cd63e8a9201d88af10408a3450f736d04c81
Swindles McCoop <65102316+swindlesmccoop@users.noreply.github.com>
2021-05-25 12:14:58 -0400

    Update skills.sh

--- Unstable Features/skills.sh
+++ Unstable Features/skills.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+
+#randomize function
+function randomize {
+paste <(printf "%s\n" *) <(printf "%s\n" * | shuf) |
+  while IFS=$'\t' read -r from to; do mv -- "$from" "$to.new"; done
+for f in *.new; do mv -- "$f" "${f%.new}"; done 
+}
+
+cd skill
+randomize
