dee32aa2d17343d3985b905fb93b811dc565ca73
Swindles McCoop <65102316+swindlesmccoop@users.noreply.github.com>
2021-06-04 11:47:35 -0400

    Update idiotproof.sh

--- idiotproof.sh
+++ idiotproof.sh
@@ -1,26 +1,15 @@
 #!/bin/sh
-
-cd ..
-cd ..
+set -e
 
 echo "Please drag and drop your data_e dump onto here":
 read dump
 
 cd "$dump"
 
-echo "Press any key to continue"
-while [ true ] ; do
-read -t 3 -n 1
-if [ $? = 0 ] ; then
-exit ;
-else
-echo "waiting for the keypress"
-fi
-done
-
-touch worked.txt
-
 if [[ -d battle ]]
 then
     echo "Getting ready for randomization..."
 fi
+
+pacman -S wget
+wget https://github.com/swindlesmccoop/P4GPC-Randomizer/releases/download/0.9/Release.zip
