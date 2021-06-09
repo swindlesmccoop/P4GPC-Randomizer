2529a1841163e3ba56c880c9f883ee64430f148a
Swindles McCoop <65102316+swindlesmccoop@users.noreply.github.com>
2021-06-09 11:32:07 -0400

    Update audio.sh

--- Unstable Features/audio.sh
+++ Unstable Features/audio.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
 set -e
-#i have no idea if any of these files are used, but we're just gonna roll with it
 
-#randomize function
-function randomize {
-paste <(printf "%s\n" *) <(printf "%s\n" * | shuf) |
-  while IFS=$'\t' read -r from to; do mv -- "$from" "$to.new"; done
-for f in *.new; do mv -- "$f" "${f%.new}"; done 
-}
-
-cd sound/adx2/comse/snd04_comse
-randomize
+#audio won't be randomized until i can find a good linux cli to actually let me automate it
